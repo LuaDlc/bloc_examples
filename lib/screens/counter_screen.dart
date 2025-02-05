a78bf7f8191d5101d2fe0f22a871d798ee0f4ea5
Luana Dias <luana.dias@colab.re>
2025-02-05 11:12:28 -0300

    fix: ajustes no counter screen

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -12,48 +12,71 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
+  late CounterBloc _counterBloc;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _counterBloc = CounterBloc();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _counterBloc.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter example'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BlocBuilder<CounterBloc, CounterStates>(builder: (context, state) {
-            return Center(
-              child: Text(
-                state.counter.toString(),
-                style: const TextStyle(fontSize: 60),
-              ),
-            );
-          }),
-          Row(
+    return BlocProvider(
+        create: (_) => _counterBloc,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Counter example'),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                  onPressed: () {
-                    context.read<CounterBloc>().add(IncrementCounter());
-                  },
-                  child: const Text('Increment')),
-              const SizedBox(
-                width: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    context.read<CounterBloc>().add(DecrementCounter());
-                  },
-                  child: const Text('Decrement')),
-              ElevatedButton(
-                  onPressed: () {
-                    context.read<CounterBloc>().add(RestartCounter());
-                  },
-                  child: const Text('restart')),
+              BlocBuilder<CounterBloc, CounterStates>(
+                  builder: (context, state) {
+                return Center(
+                  child: Text(
+                    state.counter.toString(),
+                    style: const TextStyle(fontSize: 60),
+                  ),
+                );
+              }),
+              BlocBuilder<CounterBloc, CounterStates>(
+                buildWhen: (current, previous) => false,
+                builder: (context, state) {
+                  return Row(
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {
+                            context.read<CounterBloc>().add(IncrementCounter());
+                          },
+                          child: const Text('Increment')),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            context.read<CounterBloc>().add(DecrementCounter());
+                          },
+                          child: const Text('Decrement')),
+                      ElevatedButton(
+                          onPressed: () {
+                            context.read<CounterBloc>().add(RestartCounter());
+                          },
+                          child: const Text('restart')),
+                    ],
+                  );
+                },
+              )
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        ));
   }
 }
